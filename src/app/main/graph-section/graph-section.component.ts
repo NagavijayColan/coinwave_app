48dcffda697eb5ea62f5253fc57cf7e98d31b6a6
Nagavijay <tirumalanagavijay@colanonline.com>
2018-04-30 19:09:09 +0530

    Charts integration

--- src/app/main/graph-section/graph-section.component.ts
+++ src/app/main/graph-section/graph-section.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-graph-section',
   templateUrl: './graph-section.component.html',
   styleUrls: ['./graph-section.component.css']
 })
 export class GraphSectionComponent implements OnInit {
-
+  public myOptions;
   constructor() { }
-
-  ngOnInit() {
+ 
+  ngOnInit() {debugger
+    setTimeout(function(){
+      let k = document.getElementsByClassName('amcharts-period-selector-div');
+      for(let i = 0; i < k.length ; i++){
+        k[i].setAttribute('style','display:none !important')
+      }
+     
+    },300)
+   
+    this.myOptions =[{
+    name:"vijay"
+    },
+    {
+      name:"vijay"
+      },
+      {
+        name:"vijay"
+        }]
   }
 
 }
--- src/app/main/theme-header/theme-header.component.ts
+++ src/app/main/theme-header/theme-header.component.ts
@@ -13,7 +13,14 @@ export class ThemeHeaderComponent implements OnInit {
  
 
   ngOnInit() {
-    this.hideThemesection = true;
+    if(sessionStorage.getItem('hideThemesection')){
+      this.hideThemesection = JSON.parse(sessionStorage.getItem('hideThemesection'));
+    }
+    else{
+      this.hideThemesection = true;
+     sessionStorage.setItem('hideThemesection','true')
+    }
+   
     this.desktoplists = [
         { label:'Favourite', ischecked:true },
         { label:'Coin', ischecked:true },
@@ -25,25 +32,25 @@ export class ThemeHeaderComponent implements OnInit {
         { label:'24 HR High', ischecked:true },
         { label:'24 HR Low', ischecked:true },
         { label:'24 HR Volume', ischecked:true },
-        { label:'Market Cap', ischecked:true },
-        { label:'24 HR High', ischecked:true },
-        { label:'24 HR Low', ischecked:true },
+        { label:'Circulation Supply ', ischecked:false },
+        { label:'Total Supply', ischecked:false },
+        { label:'Exchanges ', ischecked:false },
        ];
 
        this.mobilelists = [
-        { label:'Favourite', ischecked:true },
+        { label:'Favourite', ischecked:false },
         { label:'Coin', ischecked:true },
         { label:'Price', ischecked:true },
         { label:'24 HR Change', ischecked:true },
         { label:'7 Day Change', ischecked:true },
-        { label:'24 HR Volume', ischecked:true },
-        { label:'Market Cap', ischecked:true },
-        { label:'24 HR High', ischecked:true },
-        { label:'24 HR Low', ischecked:true },
-        { label:'24 HR Volume', ischecked:true },
-        { label:'Market Cap', ischecked:true },
-        { label:'24 HR High', ischecked:true },
-        { label:'24 HR Low', ischecked:true },
+        { label:'24 HR Volume', ischecked:false },
+        { label:'Market Cap', ischecked:false },
+        { label:'24 HR High', ischecked:false },
+        { label:'24 HR Low', ischecked:false },
+        { label:'24 HR Volume', ischecked:false },
+        { label:'Circulation Supply ', ischecked:false },
+        { label:'Total Supply', ischecked:false },
+        { label:'Exchanges ', ischecked:false },
        ];
   }
 
@@ -64,14 +71,12 @@ export class ThemeHeaderComponent implements OnInit {
       body.classList.add('black-theme');
       body.classList.remove('white-theme');
     }
-    // body.classList.remove("black-theme");
-    // body.classList.add("white-theme");
-    
   }
 
   themeSectionHide()
-  {
-    this.hideThemesection = !this.hideThemesection
+  {debugger
+    sessionStorage.setItem('hideThemesection','false');
+    this.hideThemesection = JSON.parse(sessionStorage.getItem('hideThemesection'));
   }
 
 }
